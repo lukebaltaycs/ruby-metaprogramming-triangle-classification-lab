af68f76fa8c9d9b6c1373f73284f67ac56984da8
Luke Baltay <luke.baltay@yale.edu>
2022-06-11 15:58:18 -0400

    commit tree e714414bffd483d250364b01dee03dbe9155b70e

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,45 @@
 class Triangle
-  # write code here
+
+  attr_accessor :side_one, :side_two, :side_three
+  
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+    @sides = [@side_one, @side_two, @side_three]
+
+    if (@side_one + @side_two <= @side_three) || (@side_one + @side_three <= @side_two) || (@side_two + @side_three <= @side_one)
+      raise TriangleError
+    elsif @sides.include?(0)
+      raise TriangleError
+    end
+  end
+
+  def how_many_equal
+    equal = 0
+    @sides.each do |side|
+      @sides.each do |lower_side|
+        if side == lower_side
+          equal += 1
+        end
+      end
+    end
+    equal
+  end
+
+  def kind
+    if how_many_equal == 9
+      return :equilateral
+    elsif how_many_equal == 5
+      return :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "the side lengths are all jacked up"
+    end
+  end
 end
